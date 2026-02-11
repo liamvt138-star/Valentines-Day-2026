722c081253875a8313726b4618b879a5a2d10c94
liamvt138-star <liamvt138@gmail.com>
2026-02-11 01:07:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -73,13 +73,13 @@ yesBtn.style.transition = "transform 0.3s ease";
 noBtn.addEventListener("click", () => {
       yesScale +=2;
 
-if(yesBtn.style.position !== "fixed") {
-    yesBtn.style.position = "fixed";
-    yesBtn.style.top = "50%";
-    yesBtn.style.left = "50%";
-    yesBtn.style.transform = 'translate(-50%, -50%) scale(${yesScale})';
-        }else{
-            yesBtn.style.transform = 'translate(-50%, -50%) scale(${yesScale})';
+    if(yesBtn.style.position !== "fixed") {
+        yesBtn.style.position = "fixed";
+        yesBtn.style.top = "50%";
+        yesBtn.style.left = "50%";
+        yesBtn.style.transform = 'translate(-50%, -50%) scale(${yesScale})';
+    }else{
+        yesBtn.style.transform = 'translate(-50%, -50%) scale(${yesScale})';
         }
 });
 
