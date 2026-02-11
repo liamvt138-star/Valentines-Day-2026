5a178d5ad8b916cdb09b14b7a1eb8b642acb55c3
liamvt138-star <liamvt138@gmail.com>
2026-02-11 00:55:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const envelope = document.getElementById("envelope-container");
 const letter = document.getElementById("letter-container");
 const noBtn = document.querySelector(".no-btn");
-const yesBtn = document.querySelector(".btn[alt='Yes']");
+const yesBtn = document.querySelector(".yes-btn");
 
 const title = document.getElementById("letter-title");
 const koalaImg = document.getElementById("letter-koala");
